b0d4d6437f4c9e5e4523cfccddd8d1b8a6416090
De <qsf44@outlook.com>
2025-01-23 17:41:27 +0800

    topbar and greeting completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,26 @@
 // app/index.tsx
-import { YStack, XStack, Text, Button } from "tamagui";
+import { YStack, XStack, Text, Button, H1 } from "tamagui";
 import { View, StyleSheet, SafeAreaView } from "react-native";
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import Feather from '@expo/vector-icons/Feather';
+import TopBar from "../components/index/topBar";
+
+const topBarColor = "grey";
+const backgroundColor = "#121212";
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.mainContent}>
         <YStack
-          backgroundColor="black"
+          backgroundColor={backgroundColor}
           width="100%"
           maxWidth={1000}
           alignSelf="center"
         >
-          {/* <Text fontSize="$4" color="white">Welcome to Tamagui + Expo Router!</Text>
-        <Button>
-          Hello
-        </Button> */}
-          <XStack style={styles.topBar} gap={30}>
-            <FontAwesome6 name="equals" size={24} color="white" style={styles.menuIcon} />
-            <Feather name="search" size={24} color="white" />
-            <Feather name="bell" size={24} color="white" />
-          </XStack>
+          {/* Top Bar */}
+          <TopBar topBarColor={topBarColor} />
+          {/*  Heading of Greeting */}
+          <View style={styles.headingArea}>
+            <H1 color="white" fontSize={40}>What's up, Olivia!</H1>
+          </View>
         </YStack>
       </View>
     </SafeAreaView>
@@ -31,17 +30,14 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
+    backgroundColor: backgroundColor,
     width: "100%",
     maxWidth: "100%",
   },
   mainContent: {
     marginHorizontal: 30,
   },
-  topBar: {
-    marginTop: 50,
+  headingArea: {
+    paddingVertical: 30,
   },
-  menuIcon: {
-    flex: 1,
-  }
 });
---
+++ components/index/topBar.tsx
@@ -0,0 +1,27 @@
+import { XStack } from "tamagui";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import Feather from "@expo/vector-icons/Feather";
+import { StyleSheet } from "react-native";
+export default function TopBar({topBarColor}: {topBarColor: string}) {
+    return (
+          <XStack style={styles.topBar} gap={30}>
+            <FontAwesome6
+              name="equals"
+              size={24}
+              color={topBarColor}
+              style={styles.menuIcon}
+            />
+            <Feather name="search" size={24} color={topBarColor} />
+            <Feather name="bell" size={24} color={topBarColor} />
+          </XStack>
+    ) 
+}
+
+const styles = StyleSheet.create({
+  topBar: {
+    marginTop: 50,
+  },
+  menuIcon: {
+    flex: 1,
+  },
+});
