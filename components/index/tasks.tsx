db70eb2c08b0af28e76a9745fbc5066db0f8856e
De <qsf44@outlook.com>
2025-01-24 12:41:06 +0800

    adding tasks sections

--- components/index/tasks.tsx
+++ components/index/tasks.tsx
@@ -1,8 +1,34 @@
 import { todos } from "@/mock/todos";
-
+import { View } from "react-native";
+import { Text, YStack, XStack, Checkbox } from "tamagui";
+import { StyleSheet, FlatList } from "react-native";
+import SectionTitle from "../common/sectionTitle";
+import { Check as CheckIcon } from '@tamagui/lucide-icons'
 export default function Tasks() {
-    const subTitleColor = "grey";
-    return (
-       <></> 
-    )
+  const title = "TODAY'S TASKS";
+  const styles = createStyles();
+  return (
+    <View style={styles.tasksContainer}>
+      <SectionTitle title={title}>
+        <XStack backgroundColor={"black"} height={70} borderRadius={20}   alignItems={"center"} gap={20}>
+          <Checkbox >
+            <Checkbox.Indicator>
+                <CheckIcon color={"white"} />
+            </Checkbox.Indicator>
+          </Checkbox>
+          <Text color={"white"} fontSize={16}>
+            CATEGORIES
+          </Text>
+        </XStack>
+      </SectionTitle>
+    </View>
+  );
+}
+
+function createStyles() {
+  return StyleSheet.create({
+    tasksContainer: {
+      marginTop: 20,
+    },
+  });
 }
